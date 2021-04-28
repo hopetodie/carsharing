7ce1734f839fca90dbd65944e77e77bce3a4996d
Алихан Гаджиев <alihangadziev@MacBook-Air-Alihan.local>
2021-04-28 16:46:54 +0600

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    $('.about-us__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        fade: true
+    });
+
+    $('.reviews__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        
+    })
+
+});
